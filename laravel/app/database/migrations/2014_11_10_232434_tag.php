06604874911dd38b24c2999e408faeff16477e18
unknown <samt@samt.st>
2014-11-11 00:33:48 +0100

    tags migration

---
+++ laravel/app/database/migrations/2014_11_10_232434_tag.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Tag extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('tags', function($table)
+		{
+			$table->increments('id');
+			$table->integer('file_id');
+			$table->string('type');
+			$table->string('value');
+
+			$table->date('created_at');
+			$table->date('updated_at');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('tags');
+	}
+
+}
