04fc3a6f50f95cd29f32d87571f322050a54e98e
unknown <samt@samt.st>
2014-11-11 21:41:54 +0100

    geo data migration and model

---
+++ laravel/app/database/migrations/2014_11_11_203837_geo_data.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class GeoData extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('geodata', function($table)
+		{
+			$table->increments('id');
+			$table->integer('file_id');
+
+			$table->integer('latitude');
+			$table->integer('longitude');
+			$table->integer('elevation');
+			$table->string('literal_locations');
+
+			$table->date('created_at');
+			$table->date('updated_at');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('geodata');
+	}
+
+}
---
+++ laravel/app/models/GeoDataModel.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class GeoDataModel extends Eloquent {
+
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'geodata';
+
+	/*
+	relations
+	*/
+}
