258610dfbf805a6d1bba8eb845ee65400b27c609
unknown <samt@samt.st>
2014-11-12 20:41:59 +0100

    search joins geo data

--- laravel/app/controllers/SearchController.php
+++ laravel/app/controllers/SearchController.php
@@ -21,47 +21,65 @@ class SearchController extends BaseController {
 		
 		$sQuery = Input::get("query");
 
+		$soFiles = DB::table("files")
+		->join("tags", function($join)
+			{
+				$join->on("files.id", "=", "tags.file_id")
+				->where("value", "=", Input::get("query"));
+			})	
+		->join("geodata", function($joinGeoData)
+		{
+			$joinGeoData->on("files.id", "=", "geodata.file_id");
+		})	
+		->where("live", "=", true)->distinct("value")
+		->orderBy("datetime", "desc")
+        ->select("files.id", "tags.value", "geodata.latitude", "geodata.longitude")
+		->get();
+
+
+		$oResults["results"] = $soFiles;
+		return Response::json($oResults);
+		/*
 		if($sQuery !== ""){
 			$soFiles = FileModel::whereHas("tags", function($q)
 				{
 					$q->where("value", "=", Input::get("query"));
-				})->where("live", "=", true)->orderBy("datetime", "desc")->get();
+				})
+			->whereHas("geodata", function($g)
+			{
+				/*$g->where("id", "=", "geodate.file_id");
+			})
+			->where("live", "=", true)
+			->orderBy("datetime", "desc")
+			->get();
 
-			$oResults["results"] = $soFiles;
+			
 
 
 			if(!Input::get("render"))
-				return Response::json($oResults);
+				
 			else
 				foreach ($soFiles as $value) {
 					echo '<img src="/thumbs/medium/'.$value->id.'.jpg"/>';
 				}
-		}
+		}*/
+		
 	}
 
 	public static function tree()
 	{		
 		// return list of unique folder with live files
-		/*
-		$soFiles = FileModel::whereHas("tags", function($q)
-			{
-				$q->where("type", "=", "uniquedirectorypath");
-			})->with(array('tags' => function($query)
-				{
-					$query->where("type", "=", "uniquedirectorypath");
-				}))->where("live", "=", true)->orderBy("datetime", "desc")->get();
-
-		*/
+		
 		$soFiles = DB::table("files")
 		->join("tags", function($join)
 			{
 				$join->on("files.id", "=", "tags.file_id")
 				->where("type", "=", "uniquedirectorypath");
-			})
+			})		
 		->where("live", "=", true)->distinct("value")
 		->orderBy("datetime", "desc")
         ->groupBy('value')
-        ->select("files.id", "tags.value")
+        ->select("files.id", "tags.value", "geodata.latitude", "geodata.longitude")
 		->get();
 
 
