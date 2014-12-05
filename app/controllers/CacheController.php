5bb02e8c0285ec9e1bc7d75328227998ad13e8c7
unknown <samt@samt.st>
2014-12-05 01:52:23 +0100

    cache controller in2

---
+++ app/controllers/CacheController.php
@@ -0,0 +1,15 @@
+<?php
+
+class CacheController extends BaseController {
+
+
+	public static function loadSearchTags()
+	{
+		// create an array of objects to be used in drop down search filter
+
+	}
+	public static function getSearchSuggestions($sTerm)
+	{
+		
+	}
+}
