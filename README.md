156606cdddd5d7f8d7046bde9560fc196185e0ac
unknown <samt@samt.st>
2014-12-15 00:47:46 +0100

    testing places processor and updated readme

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ mediadump
 =========
 
 picture &amp; video management and CDN site
+
+
+mediadump is designed to be an automated picture/video manager. for people or organisations that have too much media to keep track of effectively.
+
+mediadump aims to automatically tag your files, and make them searchable.
+
+instead of grouping your media into just folders, where files can get lost & forgotten about, mediadump can index them to keep them at your fingertips when you need to find the appropriate item.
+
+Currently indexes jpegs, and avi/mpeg4 video is planned.
+
+images are processed by
+- exif data
+- geo exif data
+- image object analysis (integration with imagga)
+
+planned
+- geo places look up
+- geo elevation look up
--- app/controllers/PlacesProcessor.php
+++ app/controllers/PlacesProcessor.php
@@ -18,10 +18,16 @@ class PlacesProcessor extends BaseController {
 
 			$bHasLatLon = false;
 
-			if(isset($oGeoData)){
-				if(isset($oGeoData->latitude) && isset($oGeoData->longitude)){
-					if($oGeoData->latitude !== 0 && $oGeoData->longitude !== 0){
+			if(isset($oGeoData))
+			{
+				if(isset($oGeoData->latitude) && isset($oGeoData->longitude))
+				{
+					if($oGeoData->latitude !== 0 && $oGeoData->longitude !== 0)
+					{
 						$bHasLatLon = true;
+					}
+				}
+			}
 
 
 			if($bHasLatLon)
@@ -30,19 +36,9 @@ class PlacesProcessor extends BaseController {
 				$sPlacesURL  = 'http://maps.googleapis.com/maps/api/geocode/json?latlng='.urlencode($oGeoData->latitude).','.urlencode($oGeoData->longitude).'&sensor=false';
                 
 
+				echo $sPlacesURL;
 
-
-
-
-
-
-
-
-
-
-
-
-				$json = file_get_contents($sPlacesURL, false, $context);
+				$json = file_get_contents($sPlacesURL);
 
 
 				list($version, $status_code, $msg) = explode(' ',$http_response_header[0], 3);
@@ -72,7 +68,9 @@ class PlacesProcessor extends BaseController {
 											if(isset($oImageResult->formatted_address))
 											{
 												if(!in_array($oImageResult->formatted_address, $saPlaces))
+												{
 													array_push($saPlaces, $oImageResult->formatted_address);
+												}
 
 											}
 											// get all distinct address components
@@ -81,9 +79,9 @@ class PlacesProcessor extends BaseController {
 												foreach($oImageResult->address_components as $jsonAddressComponent){
 
 													if(!in_array($jsonAddressComponent->long_name, $saComponents))
+													{
 														array_push($saComponents, $jsonAddressComponent->long_name);
-
-
+													}
 												}
 											}
 										}			
@@ -148,13 +146,10 @@ class PlacesProcessor extends BaseController {
 						}
 				        break;
 				    default:
-				         return "fail";
-				         break;
-				}else{
-					return "fail";
+				        return "fail";
+				        break;
 				}
-				// still here? everything was fine
-				return "ok";				
+
 				
 			}else{
 				$eProcessingFailed = new ErrorModel();
@@ -164,6 +159,8 @@ class PlacesProcessor extends BaseController {
 				$eProcessingFailed->save();
 				return "fail";
 			}
+			// still here? everything was fine
+			return "ok";
 		}
 		catch(Exception $ex)
 		{
