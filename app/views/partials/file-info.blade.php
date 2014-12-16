bcc54e2560cfefbb80a5590ee6ee881b9da735d9
unknown <samt@samt.st>
2014-12-16 01:30:11 +0100

    ui integration

--- app/views/partials/file-info.blade.php
+++ app/views/partials/file-info.blade.php
@@ -1,7 +1,30 @@
 partial view
-
 <?php
-	foreach($filedata as $data){
-		print_r($data);
+	$bGeoData = false;
+
+	if(count($filedata) > 0)
+	{
+		if(isset($filedata[0]->latitude) && isset($filedata[0]->longitude))
+			if(($filedata[0]->latitude !== 0) && ($filedata[0]->longitude !== 0))
+				$bGeoData = true;
 	}
+
 ?>
+	<div class="row">
+	@if($bGeoData)
+		{{-- show map and tags --}}
+		<div class="col-xs-6">
+			<img src="https://maps.googleapis.com/maps/api/staticmap?center={{$filedata[0]->longitude}},{{$filedata[0]->longitude}}&zoom=13&size=600x300&maptype=roadmap" />
+		</div>
+		<div class="col-xs-6">
+	@else
+		{{-- only tags --}}
+		<div class="col-xs-6">
+	@endif
+	@foreach($filedata as $data)
+		<span class="label label-default">{{$data->type}} : {{$data->value}}</span>	
+	@endforeach
+
+		</div>
+
+</div>
