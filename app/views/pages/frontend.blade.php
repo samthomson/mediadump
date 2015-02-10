09a964866dda460154fd9ec86941d70e8ef5a67c
unknown <samt@samt.st>
2015-02-10 19:31:14 +0100

    parameter for ga

--- app/views/pages/frontend.blade.php
+++ app/views/pages/frontend.blade.php
@@ -153,7 +153,7 @@
 		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-		  ga('create', {{Helper::_AppProperty('sGATrackingCode')}}, 'auto');
+		  ga('create', '{{Helper::_AppProperty('sGATrackingCode')}}', 'auto');
 		  ga('send', 'pageview');
 
 		</script>
