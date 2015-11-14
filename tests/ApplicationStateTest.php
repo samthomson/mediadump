7c58ee65ceb238fa39190aa63be1297e5d0ace57
unknown <samt@samt.st>
2015-11-14 22:44:54 +0000

    unit tests on mediadump state

---
+++ tests/ApplicationStateTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+use App\Http\Controllers;
+use App\Http\Controllers\MediaDumpController;
+use App\User;
+
+class ApplicationStateTest extends TestCase
+{
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testPingStateEmpty()
+    {
+        //$oJson = json_decode(MediaDumpController::ping());
+
+        $oJson = json_decode(MediaDumpController::ping()->getContent());
+
+        $this->assertEquals($oJson->md_state, "clean");
+    }
+
+    public function testPingStateSetUp()
+    {
+        //$oJson = json_decode(MediaDumpController::ping());
+
+        $oUser = new User;
+        $oUser->name = "test";
+        $oUser->email = "test@test.test";
+        $oUser->password = "testpass";
+        $iTempUser = $oUser->save();
+
+        $oJson = json_decode(MediaDumpController::ping()->getContent());
+
+        $oUser->delete();
+
+        $this->assertEquals($oJson->md_state, "setup");
+    }
+}
