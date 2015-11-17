2143c82124041a00563cc22ea5293089684c8146
unknown <samt@samt.st>
2015-11-17 17:38:00 +0000

    font stuff?

--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -5,6 +5,15 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
         <link rel="stylesheet" href="{{ elixir('css/all.css') }}">
+
+        <!-- font font(s) -->
+
+        <link rel='stylesheet' id='g_font-css'  href='http://fonts.googleapis.com/css?family=Noto+Sans%3A400%2C700%2C400italic%2C700italic&#038;ver=3.5.1' type='text/css' media='all' />
+
+        <link href='http://fonts.googleapis.com/css?family=PT+Sans+Caption:700' rel='stylesheet' type='text/css'>
+
+
+        <!-- icon font(s) -->
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
       rel="stylesheet">
     </head>
@@ -14,7 +23,7 @@
             <md-progress-linear md-mode="indeterminate" ng-show="getMDApp().bSomethingLoading" id="app-load-state"></md-progress-linear>
 
             <div id="header">
-                <button ng-ckick="home()" ng-disabled="getMDApp().state == 'empty'">mediadump</button> setup: @{{getMDApp().state}}, loading: @{{getMDApp().bSomethingLoading}}, logged in: @{{getMDApp().bLoggedIn}}
+                <button ng-ckick="home()" ng-disabled="getMDApp().state == 'empty'" class="header-font">mediadump</button> setup: @{{getMDApp().state}}, loading: @{{getMDApp().bSomethingLoading}}, logged in: @{{getMDApp().bLoggedIn}}
 
                 <ul><li>login?</li></ul>
             </div>
