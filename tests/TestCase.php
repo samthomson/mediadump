7cbb2c3393958dd358c0be8664c6b06a7bc1323e
unknown <samt@samt.st>
2015-11-19 11:58:55 +0000

    testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -19,14 +19,14 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
         
         $app = require __DIR__.'/../bootstrap/app.php';
 
-        $app->loadEnvironmentFrom('.env.testing'); // specify the file to use for environment, must be run before boostrap
+        //$app->loadEnvironmentFrom('.env.testing'); // specify the file to use for environment, must be run before boostrap
 
         $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
         return $app;
     }
 
-    
+
 
     public function setUp()
     {
